268fe3a58a80de1d8fa13465e8790bf01a14c88e
Teo Itsweire <teo.itsweire@etu.univ-poitiers.fr>
2024-10-11 14:42:37 +0200

    11/10

--- src/univ/syllogismverificator/controllers/SolverController.java
+++ src/univ/syllogismverificator/controllers/SolverController.java
@@ -1,11 +1,64 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class SolverController {
+
+    @FXML
+    private MenuButton QQLGM1;
+    @FXML
+    private MenuButton QQLGM2;
+    @FXML
+    private MenuButton QQLGM3;
+
+    @FXML
+    private MenuButton QQLFM1;
+    @FXML
+    private MenuButton QQLFM2;
+    @FXML
+    private MenuButton QQLFM3;
+
+    @FXML
+    private MenuButton S11;
+    @FXML
+    private MenuButton S12;
+    @FXML
+    private MenuButton S21;
+    @FXML
+    private MenuButton S22;
+    @FXML
+    private MenuButton S31;
+    @FXML
+    private MenuButton S32;
+
+
     @FXML
     public void initialize() {
+        initMenuItems();
         loadMenuItemsFromJson();
+    }
+
+    private void initMenuItems() {
+        for (MenuItem mi : QQLGM1.getItems()){
+            mi.setOnAction(event -> QQLGM1.setText(mi.getText()));
+        }
+        for (MenuItem mi : QQLGM2.getItems()){
+            mi.setOnAction(event -> QQLGM2.setText(mi.getText()));
+        }
+        for (MenuItem mi : QQLGM3.getItems()){
+            mi.setOnAction(event -> QQLGM3.setText(mi.getText()));
+        }
+        for (MenuItem mi : QQLFM1.getItems()){
+            mi.setOnAction(event -> QQLFM1.setText(mi.getText()));
+        }
+        for (MenuItem mi : QQLFM2.getItems()){
+            mi.setOnAction(event -> QQLFM2.setText(mi.getText()));
+        }
+        for (MenuItem mi : QQLFM3.getItems()){
+            mi.setOnAction(event -> QQLFM3.setText(mi.getText()));
+        }
     }
 
     private void loadMenuItemsFromJson() {
