c0a190bb1c4e12f91d5bc19d81636cf0fc2dd8ae
FERRY ANTONY <antony.ferry@etu.univ-poitiers.fr>
2024-10-11 08:00:06 +0200

    return null => throw new UnsupportedException

--- src/univ/syllogismverificator/models/Polysyllogism.java
+++ src/univ/syllogismverificator/models/Polysyllogism.java
@@ -9,5 +9,7 @@ public class Polysyllogism {
     public final List<Proposition> propositions = new ArrayList<>();
 
     //TODO : Faire la méthode accept et le retour de RuleResult.
-    public RuleResult accept(Rule rule) {return null;}
+    public RuleResult accept(Rule rule) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
