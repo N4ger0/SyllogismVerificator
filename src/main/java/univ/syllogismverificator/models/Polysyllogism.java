93ba780d66a390ce8418c7e27565be1d9763e774
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2024-10-15 09:31:59 +0200

    Add conclusion attribute on pollysillogism

--- src/main/java/univ/syllogismverificator/models/Polysyllogism.java
+++ src/main/java/univ/syllogismverificator/models/Polysyllogism.java
@@ -3,14 +3,17 @@ package univ.syllogismverificator.models;
 import univ.syllogismverificator.models.rules.Rule;
 import univ.syllogismverificator.models.rules.RuleResult;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Polysyllogism {
     private final List<Proposition> propositions;
+    private final Proposition conclusion;
 
     public Polysyllogism(List<Proposition> propositions){
-        this.propositions = propositions;
+        this.propositions = new ArrayList<>(propositions);
+        this.conclusion = this.propositions.removeLast();
     }
 
     /**
@@ -24,5 +27,13 @@ public class Polysyllogism {
 
     public Stream<Proposition> stream(){
         return propositions.stream();
+    }
+
+    public List<Proposition> getPropositions() {
+        return new ArrayList<>(propositions);
+    }
+
+    public Proposition getConclusion() {
+        return conclusion;
     }
 }
