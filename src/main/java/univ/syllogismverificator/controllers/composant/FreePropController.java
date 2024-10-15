c2ec0fc9f32b447a40878d254be32553d7dc861b
Oregan <oregan.hardy@etu.univ-poitiers.fr>
2024-10-15 09:25:58 +0200

    submenu pour mode avancé

--- src/main/java/univ/syllogismverificator/controllers/composant/FreePropController.java
+++ src/main/java/univ/syllogismverificator/controllers/composant/FreePropController.java
@@ -1,11 +1,20 @@
 package univ.syllogismverificator.controllers.composant;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.util.Pair;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +23,12 @@ import java.util.Map;
  * Elle contient les informations necessaire a la resolution du syllogisme.
  */
 public class FreePropController {
+    private ArrayList<Pair<String, ArrayList<String>>> QQLList = new ArrayList<>() ;
+    private String classe ;
+
+    public String getClasse() {
+        return classe ;
+    }
     /** Le texte indiquant le numero de la premisse.*/
     @FXML
     private Text text;
@@ -40,8 +55,29 @@ public class FreePropController {
 
     @FXML
     public void initialize() {
+        loadMenuItemsFromJson();
         initText();
         initMenuItems();
+    }
+
+    private void loadMenuItemsFromJson() {
+        try {
+            Object o = new JSONParser().parse(new FileReader("src/main/resources/data/quanqual.json"));
+            JSONArray j = (JSONArray) o;
+            for (Object object : j) {
+                JSONObject myObj = (JSONObject) object;
+                String tempClasse = (String) myObj.get("value");
+                JSONArray j2 = (JSONArray) myObj.get("array");
+                ArrayList<String> tempList = new ArrayList<>() ;
+                for(Object object2 : j2) {
+                    JSONObject myObj2 = (JSONObject) object2 ;
+                    tempList.add((String) myObj2.get("key")) ;
+                }
+                QQLList.add(new Pair<>((String) myObj.get("value"), tempList));
+            }
+        } catch (ParseException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void initText(){
@@ -51,8 +87,18 @@ public class FreePropController {
 
 
     private void initMenuItems() {
-        for (MenuItem mi : freeQQL.getItems()){
-            mi.setOnAction(event -> freeQQL.setText(mi.getText()));
+        for(Pair<String, ArrayList<String>> p : QQLList){
+            Menu mySubMenu = new Menu(p.getKey()) ;
+            for(String s : p.getValue()) {
+                MenuItem tempItem = new MenuItem(s) ;
+                mySubMenu.getItems().add(tempItem) ;
+                tempItem.setOnAction(event -> {
+                    freeQQL.setText(tempItem.getText()) ;
+                    classe = mySubMenu.getText() ;
+                });
+            }
+
+            freeQQL.getItems().add(mySubMenu);
         }
         for (MenuItem mi : freeTerme1.getItems()){
             mi.setOnAction(event -> freeTerme1.setText(mi.getText()));
